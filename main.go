83d5793b47cc859d3089c49056364b22b1957999
Litong Deng <litong.deng@compass.com>
2023-02-27 21:01:51 +0800

    add: query for single row
    Signed-off-by: Litong Deng <litong.deng@compass.com>

--- main.go
+++ main.go
@@ -65,6 +65,12 @@ func main() {
 		log.Fatal(err)
 	}
 	log.Printf("Albums found: %v\n", albums)
+
+	alb, err := albumById(2)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Album found: %v\n", alb)
 }
 
 type Album struct {
@@ -82,7 +88,11 @@ func albumsByArtist(name string) ([]Album, error) {
 	if err != nil {
 		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
 	}
-	defer rows.Close()
+	defer func(rows *sql.Rows) {
+		if err := rows.Close(); err != nil {
+			log.Println(err)
+		}
+	}(rows)
 
 	for rows.Next() {
 		var alb Album
@@ -95,4 +105,17 @@ func albumsByArtist(name string) ([]Album, error) {
 		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
 	}
 	return albums, nil
+}
+
+// albumById queries for the album with the specific ID.
+func albumById(id int64) (Album, error) {
+	var alb Album
+	row := db.QueryRow("SELECT * FROM album WHERE id = ?", id)
+	if err := row.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
+		if err == sql.ErrNoRows {
+			return alb, fmt.Errorf("albumById %d: no such album", id)
+		}
+		return alb, fmt.Errorf("albumById %d: %v", id, err)
+	}
+	return alb, nil
 }
