34867a7abb1bdbb4295f0fe946ad612324e6669f
Litong Deng <litong.deng@compass.com>
2023-02-27 21:34:34 +0800

    maint: arrange package structure
    Signed-off-by: Litong Deng <litong.deng@compass.com>

--- main.go
+++ main.go
@@ -1,78 +1,32 @@
-// This tutorial introduce the basics of accessing a relational database with Go
-// and its database/sql package in the standard library.
-// The database/sql package includes types and functions for connecting to databases,
-// execution transactions, canceling an operation in progress, and more.
-// For more details on using the database/sql package, see https://go.dev/doc/database.
-// Sections:
-// 	1.Set up a database.
-// 	2.Import the database driver.
-// 	3.Get a database handle and connect.
-// 	4.Query for multiple rows.
-// 	5.Query for a single row.
-//  6.Add data.
 package main
 
 import (
-	"database/sql"
+	"db/basic"
 	"fmt"
-	"github.com/go-sql-driver/mysql"
 	"log"
-	"os"
 )
 
-var (
-	dbName       = "recordings"
-	dbAddr       = "127.0.0.1:3306"
-	dbUser       = "DB_USER"
-	dbPassword   = "DB_PASSWORD"
-	dbDriverName = "mysql"
-	db           *sql.DB
-)
+func showBasicDB() {
+	basic.OpenDB()
+	defer basic.CloseDB()
+	basic.PingDB()
 
-// main
-// DB_USER=root DB_PASSWORD=12345678 go run .
-func main() {
-	// Capture connection properties.
-	cfg := mysql.Config{
-		User:   os.Getenv(dbUser),
-		Passwd: os.Getenv(dbPassword),
-		Net:    "tcp",
-		Addr:   dbAddr,
-		DBName: dbName,
-	}
-	// Get a database handle
-	var err error
-	// root:12345678@tcp(127.0.0.1:3306)/recordings?allowNativePasswords=false&checkConnLiveness=false&maxAllowedPacket=0
-	log.Println("config data source name", cfg.FormatDSN())
-	db, err = sql.Open(dbDriverName, cfg.FormatDSN())
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer func() {
-		if err := db.Close(); err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println("Closed!")
-	}()
-
-	if err = db.Ping(); err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("Connected!")
-
-	albums, err := albumsByArtist("John Coltrane")
+	// basic query for multiple rows
+	albums, err := basic.AlbumsByArtist("John Coltrane")
 	if err != nil {
 		log.Fatal(err)
 	}
 	log.Printf("Albums found: %v\n", albums)
 
-	alb, err := albumById(2)
+	// basic query for single row
+	alb, err := basic.AlbumById(2)
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Printf("Album found: %v\n", alb)
 
-	albId, err := addAlbum(Album{
+	// basic add data
+	albId, err := basic.AddAlbum(basic.Album{
 		Title:  "The Modern Sound of Betty Carter",
 		Artist: "Betty Carter",
 		Price:  49.99,
@@ -81,68 +35,17 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Printf("ID of added album: %v\n", albId)
-}
 
-type Album struct {
-	ID     int64
-	Title  string
-	Artist string
-	Price  float32
-}
-
-// albumsByArtist queries for albums that have the specific artist name
-func albumsByArtist(name string) ([]Album, error) {
-	var albums []Album
-
-	rows, err := db.Query("SELECT * FROM album WHERE artist = ?", name)
+	alb.Price *= 2
+	row, err := basic.UpdateAlbum(alb)
 	if err != nil {
-		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+		log.Fatal(err)
 	}
-	defer func(rows *sql.Rows) {
-		if err := rows.Close(); err != nil {
-			log.Println(err)
-		}
-	}(rows)
-
-	for rows.Next() {
-		var alb Album
-		if err := rows.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
-			return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
-		}
-		albums = append(albums, alb)
-	}
-	if err := rows.Err(); err != nil {
-		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
-	}
-	return albums, nil
+	log.Printf("Album updated row: %v\n", row)
 }
 
-// albumById queries for the album with the specific ID.
-func albumById(id int64) (Album, error) {
-	var alb Album
-	row := db.QueryRow("SELECT * FROM album WHERE id = ?", id)
-	if err := row.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
-		if err == sql.ErrNoRows {
-			return alb, fmt.Errorf("albumById %d: no such album", id)
-		}
-		return alb, fmt.Errorf("albumById %d: %v", id, err)
-	}
-	return alb, nil
-}
-
-// addAlbum adds the specific album to the database,
-// returning the album ID of the new entry
-func addAlbum(alb Album) (int64, error) {
-	result, err := db.Exec("INSERT INTO album (title, artist, price) VALUES (?, ?, ?)",
-		alb.Title, alb.Artist, alb.Price)
-	if err != nil {
-		return 0, fmt.Errorf("addAlbum: %v", err)
-	}
-
-	id, err := result.LastInsertId()
-	if err != nil {
-		return 0, fmt.Errorf("addAlbum: %v", err)
-	}
-
-	return id, nil
+// main
+// DB_USER=root DB_PASSWORD=12345678 go run .
+func main() {
+	showBasicDB()
 }
